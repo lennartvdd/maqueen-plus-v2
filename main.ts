feb7478ec69337f7f7bde985d802d1da1c2dc260
Lennart van den Dool <lennartvdd@gmail.com>
2023-08-19 17:40:28 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    D = !(D)
+})
 let direction = 0
 let distance = 0
 let n = 0
+let R = 0
+let D = false
 DFRobotMaqueenPlusV2.init()
 DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eAllLed, MyEnumSwitch.eOpen)
 DFRobotMaqueenPlusV2.setBrightness(100)
-let R = 0
 let G = 1
 let B = 2
 let P = 3
@@ -34,23 +38,66 @@ basic.forever(function () {
         P = 0
     }
     n += 1
-    basic.pause(1000)
+    if (!(D)) {
+        basic.pause(1000)
+    }
 })
 basic.forever(function () {
-    distance = DFRobotMaqueenPlusV2.readUltrasonic(DigitalPin.P13, DigitalPin.P14)
-    direction = randint(1, 2)
-    if (distance < 30 && distance != 0) {
-        if (direction == 1) {
-            DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 0)
-            DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 0)
-            basic.pause(1000)
-        }
-        if (direction == 2) {
-            DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 0)
-            DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 0)
-            basic.pause(1000)
+    distance = DFRobotMaqueenPlusV2.readUltrasonic(DigitalPin.P1, DigitalPin.P2)
+    // left = 1, //right = 2
+    if (D) {
+        if (distance < 50 && distance != 0) {
+            direction = randint(1, 2)
+            if (direction == 1) {
+                DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eBackward, 50)
+                DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 50)
+                basic.pause(1000)
+            }
+            if (direction == 2) {
+                // right
+                DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 50)
+                DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eBackward, 50)
+                basic.pause(1000)
+            }
+            DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
+        } else {
+            direction = 0
+            DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eForward, 50)
         }
     } else {
-        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eForward, 0)
+        DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
+    }
+})
+basic.forever(function () {
+    if (D) {
+        if (direction == 1) {
+            basic.showLeds(`
+                . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+                `)
+        } else if (direction == 2) {
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                `)
+        } else if (distance < 50 && distance != 0) {
+            basic.showIcon(IconNames.Tortoise)
+        } else {
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # . # . #
+                . . # . .
+                . . # . .
+                `)
+        }
+    } else {
+        basic.showIcon(IconNames.Target)
     }
 })
